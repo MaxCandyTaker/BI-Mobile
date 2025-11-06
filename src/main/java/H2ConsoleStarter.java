b260a348fa24060f86bc837d2ead3df411157d0e
LeoK1110 <Leonard.koechling@hotmail.com>
2025-11-06 13:22:57 +0100

    Update H2ConsoleStarter.java

--- src/main/java/H2ConsoleStarter.java
+++ src/main/java/H2ConsoleStarter.java
@@ -2,9 +2,9 @@ import org.h2.tools.Server;
 
 public class H2ConsoleStarter {
     public static void main(String[] args) throws Exception {
-        // Start H2 web console on port 8082
-        // Server.createWebServer("-web", "-webAllowOthers", "-webPort", "8082").start();
-        // System.out.println("H2 console started at http://localhost:8082");
+        //->> "alter" Code      Start H2 web console on port 8082
+        //                      Server.createWebServer("-web", "-webAllowOthers", "-webPort", "8082").start();
+        //                      System.out.println("H2 console started at http://localhost:8082");
 
         try {
             Connection conn = DriverManager.getConnection("jdbc:h2:~/bi_mobile_db", "sa", "");
